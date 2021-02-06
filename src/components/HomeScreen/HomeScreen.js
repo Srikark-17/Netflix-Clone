496ee73cc2a0b5f3bee3425418fa48ad5cc803d8
Srikark-17 <srikar.sgk@gmail.com>
2021-02-06 12:49:57 -0600

    Finished Rows

--- src/components/HomeScreen/HomeScreen.js
+++ src/components/HomeScreen/HomeScreen.js
@@ -1,13 +1,26 @@
 import React from "react";
 import NavigationBar from "./NavigationBar/NavigationBar";
 import Banner from "./Banner/Banner";
+import Row from "./Row/Row";
+import requests from "../../fetchAPI/requests/Requests";
 
 const HomeScreen = () => {
   return (
     <div className="homeScreen">
       <NavigationBar />
       <Banner />
-      {/* Row */}
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 };
